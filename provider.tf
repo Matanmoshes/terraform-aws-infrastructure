711846e28fb5a5c362b97044ccba53804a6beeb0
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-05 22:37:03 +0300

    new update

--- provider.tf
+++ provider.tf
@@ -6,5 +6,5 @@ provider "aws" {
 # Create an SSH key pair resource using the existing public key
 resource "aws_key_pair" "webserver-key" {
   key_name   = "webserver-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("${path.module}/id_rsa.pub")
 }
